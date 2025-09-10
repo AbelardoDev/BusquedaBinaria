04743038dd8504cfe35b1db043d5385dcca64cbd
Abelardo <aljuniors99@gmail.com>
2025-09-10 16:59:35 -0500

    Método de busqueda

--- src/main/java/model/Busqueda.java
+++ src/main/java/model/Busqueda.java
@@ -1,4 +1,14 @@
 package model;
 
 public class Busqueda {
+    // Método de búsqueda secuencial para encontrar un libro por su código
+    public int busquedaSecuencial(Libro[] libros, int codigo) {
+        int n = libros.length;
+        for (int i = 0; i < n; i++) {
+            if (libros[i] != null && libros[i].getCodigo() == codigo) {
+                return i; // Retorna la posición si se encuentra el libro
+            }
+        }
+        return -1; // Retorna -1 si no se encuentra el libro
+    }
 }
